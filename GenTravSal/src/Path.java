b0dda5a8fd4b833bb78dee92b0069632d9381df2
Paweł Stępień <pvl.staven@gmail.com>
2014-05-05 02:23:08 +0200

    Foramtting

--- GenTravSal/src/Path.java
+++ GenTravSal/src/Path.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 class Path {
-   public CityGraph graph;
-   public ArrayList<Integer> nodes;
-   
-   public Path(CityGraph g, String p) {
-      this.graph = g;
-      this.nodes = new ArrayList<Integer>();
-      for (String s : p.split("[ \t\n]*->[ \t\n]*")) {
-         this.append(Integer.parseInt(s));
-      }
-   }
-   
-   public void append(int n) {
-   City c = new City(n);
-      if (!graph.containsVertex(c)) {
-         String err = String.format("can't append %d to path: no such node in graph", n);
-         throw new RuntimeException(err);
-      }
-      this.nodes.add(n);
-   }
-   
-   public void render() {
-   }
+    public CityGraph graph;
+    public ArrayList<Integer> nodes;
+    
+    public Path(CityGraph g, String p) {
+        this.graph = g;
+        this.nodes = new ArrayList<Integer>();
+        for (String s : p.split("[ \t\n]*->[ \t\n]*")) {
+            this.append(Integer.parseInt(s));
+        }
+    }
+    
+    public void append(int n) {
+    City c = new City(n);
+        if (!graph.containsVertex(c)) {
+            String err = String.format("can't append %d to path: no such node in graph", n);
+            throw new RuntimeException(err);
+        }
+        this.nodes.add(n);
+    }
 }
