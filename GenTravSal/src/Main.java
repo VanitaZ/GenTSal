8d9eb9ad9373bf03c5364fafc7f687634170bdc2
VanitaZ <qwarety@gmail.com>
2014-06-08 18:22:59 +0200

    chart added

--- GenTravSal/src/Main.java
+++ GenTravSal/src/Main.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,16 +5,18 @@ public class Main {
     	for (int i = 0; i < 1000; i++) {
     		cities[i] = City.random();
     	}
-    	
+
+        Chart ch = new Chart();
+
         Population pop = new Population(cities);
         pop.init(10);
         for (int i = 0; i <= 4000; i++) {
         	pop.iterate();
-        	if (i % 1000 == 0) {
-        		int[] arr = pop.paths.get(0);
-        		System.out.printf("iteration %d: %f\n", i, pop.pathScore(arr));
-        	}
+            if (i % 100 == 0)
+            ch.addSeries(i,pop.pathScore(pop.paths.get(0))-5,pop.pathScore(pop.paths.get(4)),pop.pathScore(pop.paths.get(9))+5);
         }
+
+
         
         /*int[] arr = pop.paths.get(0);
         for (int i : arr) {
