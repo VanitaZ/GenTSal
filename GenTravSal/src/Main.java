926bb6c2b318db08379fc8d0ad08a4c6e552d813
VanitaZ <qwarety@gmail.com>
2014-06-10 00:19:16 +0200

    visualization

--- GenTravSal/src/Main.java
+++ GenTravSal/src/Main.java
@@ -1,23 +1,25 @@
 
 public class Main {
     public static void main(String[] args) {
-    	City[] cities = new City[1000];
-    	for (int i = 0; i < 1000; i++) {
+        GraphExplorer gr = new GraphExplorer();
+    	City[] cities = new City[10];
+    	for (Integer i = 0; i < 10; i++) {
     		cities[i] = City.random();
     	}
 
-        Chart ch = new Chart();
+        //Chart ch = new Chart();
 
         Population pop = new Population(cities);
         pop.init(10);
         for (int i = 0; i <= 4000; i++) {
         	pop.iterate();
-            if (i % 100 == 0)
-            ch.addSeries(i,pop.pathScore(pop.paths.get(0))-5,pop.pathScore(pop.paths.get(4)),pop.pathScore(pop.paths.get(9))+5);
+            //if (i % 100 == 0)
+            //ch.addSeries(i,pop.pathScore(pop.paths.get(0)),pop.pathScore(pop.paths.get(4)),pop.pathScore(pop.paths.get(9)));
         }
 
 
-        
+        gr.createGraph(cities,pop,10);
+
         /*int[] arr = pop.paths.get(0);
         for (int i : arr) {
         	System.out.println(i);
